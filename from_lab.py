29b7cfd1d209cdfde93d4e0b8e5b87646db68248
tonyumruk <151631497+tonyumruk@users.noreply.github.com>
2024-01-18 09:11:29 +0100

    Update from_lab.py
    aa

--- from_lab.py
+++ from_lab.py
@@ -1,1 +1,17 @@
-value = 1 + 2
+
+numbers = [5, 6, 7, 8]
+length = len(numbers)
+len(numbers)
+
+total_operations = 0
+for counter in range(lenght-1):
+    for i in range(lenght - 1):
+        total_operations += 1
+    if numbers[1] > numbers[i+1]:
+        old_value = numbers[i]
+        numbers[i] = numbers[i+1]
+        numbers[i+1] = old_value
+        
+print(total_operations)
+print(lenght)
+print(numbers)
