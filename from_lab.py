6ae1542eb14f990b3c629ad980df191fdbc59205
tonyumruk <151631497+tonyumruk@users.noreply.github.com>
2024-01-18 12:04:12 +0100

    Update from_lab.py
    aa

--- from_lab.py
+++ from_lab.py
@@ -1,17 +1,16 @@
-
 numbers = [5, 6, 7, 8]
 length = len(numbers)
-len(numbers)
 
 total_operations = 0
-for counter in range(lenght-1):
-    for i in range(lenght - 1):
+
+for counter in range(length - 1):
+    for i in range(length - 1):
         total_operations += 1
-    if numbers[1] > numbers[i+1]:
-        old_value = numbers[i]
-        numbers[i] = numbers[i+1]
-        numbers[i+1] = old_value
-        
-print(total_operations)
-print(lenght)
-print(numbers)
+        if numbers[i] > numbers[i+1]:
+            old_value = numbers[i]
+            numbers[i] = numbers[i+1]
+            numbers[i+1] = old_value
+
+print("Total operations:", total_operations)
+print("Length:", length)
+print("Sorted numbers:", numbers)
